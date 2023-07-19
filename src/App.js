48deb4960694f3857e2e49cdb9c64ddd59988b66
wangtao <1453300745@qq.com>
2023-07-19 10:32:46 +0800

    feat: init

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="/"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -22,4 +18,3 @@ function App() {
   );
 }
 
-export default App;
